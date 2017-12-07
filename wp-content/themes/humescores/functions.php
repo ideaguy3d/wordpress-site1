0c5dbbd320e90be93494514838a63e8dfb152d2c
Julius Alvarado <angularjs.uiux@gmail.com>
2017-12-07 00:29:23 -0800

    added a header

--- wp-content/themes/humescores/functions.php
+++ wp-content/themes/humescores/functions.php
@@ -82,31 +82,54 @@ function humescores_fonts_url() {
      * supported by Source Sans Pro and PT Serif, translate this to 'off'. Do not translate
      * into your own language.
      */
-    $source_sans_pro = _x( 'on', 'Source Sans Pro font: on or off', 'humescores' );
-    $pt_serif = _x( 'on', 'PT Serif font: on or off', 'humescores' );
+    $source_sans_pro = _x('on', 'Source Sans Pro font: on or off', 'humescores');
+    $pt_serif = _x('on', 'PT Serif font: on or off', 'humescores');
 
     $font_families = array();
 
-    if ( 'off' !== $source_sans_pro ) {
+    if ('off' !== $source_sans_pro) {
         $font_families[] = 'Source Sans Pro:400,400i,700,900';
     }
 
-    if ( 'off' !== $pt_serif) {
+    if ('off' !== $pt_serif) {
         $font_families[] = 'PT Serif:400,400i,700,700i';
     }
 
     // if either array has the key 'on' do something
-    if ( in_array( 'on', array($source_sans_pro, $pt_serif) ) ) {
+    if (in_array('on', array($source_sans_pro, $pt_serif))) {
         $query_args = array(
-            'family' => urlencode( implode( '|', $font_families ) ),
-            'subset' => urlencode( 'latin,latin-ext' ),
+            'family' => urlencode(implode('|', $font_families)),
+            'subset' => urlencode('latin,latin-ext'),
         );
 
-        $fonts_url = add_query_arg( $query_args, 'https://fonts.googleapis.com/css' );
+        $fonts_url = add_query_arg($query_args, 'https://fonts.googleapis.com/css');
     }
 
     return esc_url_raw($fonts_url);
 }
+
+/**
+ * Add preconnect for Google Fonts.
+ *
+ * @since Twenty Seventeen 1.0
+ *
+ * @param array $urls URLs to print for resource hints.
+ * @param string $relation_type The relation type the URLs are printed.
+ * @return array $urls           URLs to print for resource hints.
+ */
+function humescores_resource_hints($urls, $relation_type) {
+    if (wp_style_is('humescores-fonts', 'queue')
+            && 'preconnect' === $relation_type) {
+        $urls[] = array (
+            'href' => 'https://fonts.gstatic.com',
+            'crossorigin',
+        );
+    }
+
+    return $urls;
+}
+
+add_filter('wp_resource_hints', 'humescores_resource_hints', 10, 2);
 
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
